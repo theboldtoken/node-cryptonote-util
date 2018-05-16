6580b2f39a2ea93720cc2eefcb7445b07601a25d
Helder Garcia <helder.garcia@gmail.com>
2018-05-15 21:47:34 -0300

    Still working on blob problem

--- src/main.cc
+++ src/main.cc
@@ -66,13 +66,8 @@ static bool mergeBlocks(const cryptonote::block& block1, cryptonote::block& bloc
 }
 
 static bool construct_parent_block(const cryptonote::block& b, cryptonote::block& parent_block) {
-    if (b.major_version >= BLOCK_MAJOR_VERSION_5) {
-		    parent_block.major_version = b.major_version;
-		    parent_block.minor_version = 1;
-    } else {
-		    parent_block.major_version = 1;
-		    parent_block.minor_version = 0;
-    }
+    parent_block.major_version = 1;
+    parent_block.minor_version = 0;
     parent_block.timestamp = b.timestamp;
     parent_block.prev_id = b.prev_id;
     parent_block.nonce = b.parent_block.nonce;
@@ -101,7 +96,7 @@ Handle<Value> convert_blob(const Arguments& args) {
     if (!parse_and_validate_block_from_blob(input, b))
         return except("Failed to parse block");
 
-    if (b.major_version < BLOCK_MAJOR_VERSION_3) {
+    if (b.major_version < BLOCK_MAJOR_VERSION_2) {
         if (!get_block_hashing_blob(b, output))
             return except("Failed to create mining block");
     } else {
@@ -169,7 +164,7 @@ Handle<Value> construct_block_blob(const Arguments& args) {
 
     b.nonce = nonce;
 
-    if (b.major_version >= BLOCK_MAJOR_VERSION_3) {
+    if (b.major_version >= BLOCK_MAJOR_VERSION_2) {
         block parent_block;
         b.parent_block.nonce = nonce;
         if (!construct_parent_block(b, parent_block))
